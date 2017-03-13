0c807865fc740cefe4361ee11f13628bda914f40
wushuai <shuai.wu2@tqmall.com>
2017-03-13 20:33:11 +0800

    部分代码格式化

--- src/main/java/org/ihansen/mbp/extend/dbSupport/MysqlSupport.java
+++ src/main/java/org/ihansen/mbp/extend/dbSupport/MysqlSupport.java
@@ -11,113 +11,113 @@ import org.mybatis.generator.api.dom.xml.TextElement;
 import org.mybatis.generator.api.dom.xml.XmlElement;
 
 public class MysqlSupport implements DBSupport {
-	/**
-	 * 向&lt;mapper&gt;的子节点中添加内容支持批量和分页查询的sql代码块
-	 * 
-	 * @author 吴帅
-	 * @parameter @param document
-	 * @parameter @param introspectedTable
-	 * @createDate 2015年9月29日 上午10:20:11
-	 */
-	@Override
-	public void sqlDialect(Document document, IntrospectedTable introspectedTable) {
-		XmlElement parentElement = document.getRootElement();
-		// 1.产生mysql分页的limit条件 TODO limit分页方式待优化
-		XmlElement paginationSuffixElement = new XmlElement("sql");
-		paginationSuffixElement.addAttribute(new Attribute("id", "MysqlDialectSuffix"));
-		XmlElement pageEnd = new XmlElement("if");
-		pageEnd.addAttribute(new Attribute("test", "pageHelper != null"));
-		pageEnd.addElement(new TextElement("<![CDATA[ limit #{pageHelper.begin}, #{pageHelper.length}]]>"));
-		paginationSuffixElement.addElement(pageEnd);
-		parentElement.addElement(paginationSuffixElement);
+    /**
+     * 向&lt;mapper&gt;的子节点中添加内容支持批量和分页查询的sql代码块
+     *
+     * @author 吴帅
+     * @parameter @param document
+     * @parameter @param introspectedTable
+     * @createDate 2015年9月29日 上午10:20:11
+     */
+    @Override
+    public void sqlDialect(Document document, IntrospectedTable introspectedTable) {
+        XmlElement parentElement = document.getRootElement();
+        // 1.产生mysql分页的limit条件 TODO limit分页方式待优化
+        XmlElement paginationSuffixElement = new XmlElement("sql");
+        paginationSuffixElement.addAttribute(new Attribute("id", "MysqlDialectSuffix"));
+        XmlElement pageEnd = new XmlElement("if");
+        pageEnd.addAttribute(new Attribute("test", "pageHelper != null"));
+        pageEnd.addElement(new TextElement("<![CDATA[ limit #{pageHelper.begin}, #{pageHelper.length}]]>"));
+        paginationSuffixElement.addElement(pageEnd);
+        parentElement.addElement(paginationSuffixElement);
 
-		// 2.增加批量插入的xml配置
-		addBatchInsertXml(document, introspectedTable);
-	}
+        // 2.增加批量插入的xml配置
+        addBatchInsertXml(document, introspectedTable);
+    }
 
-	/**
-	 * 增加批量插入的xml配置
-	 * 
-	 * @author 吴帅
-	 * @parameter @param document
-	 * @parameter @param introspectedTable
-	 * @createDate 2015年8月9日 下午6:57:43
-	 */
-	@Override
-	public void addBatchInsertXml(Document document, IntrospectedTable introspectedTable) {
-		List<IntrospectedColumn> columns = introspectedTable.getAllColumns();
-		//获得要自增的列名
-		String incrementField = introspectedTable.getTableConfiguration().getProperties().getProperty("incrementField");
-		if(incrementField!=null){
-			incrementField = incrementField.toUpperCase();
-		}
-		StringBuilder dbcolumnsName = new StringBuilder();
-		StringBuilder javaPropertyAndDbType = new StringBuilder();
-		for (IntrospectedColumn introspectedColumn : columns) {
-			String columnName = introspectedColumn.getActualColumnName();
-			if(!columnName.toUpperCase().equals(incrementField)){//不是自增字段的才会出现在批量插入中
-				dbcolumnsName.append(columnName + ",");
-				javaPropertyAndDbType.append("#{item." + introspectedColumn.getJavaProperty() + ",jdbcType=" + introspectedColumn.getJdbcTypeName() + "},");
-			}
-		}
-		
-		XmlElement insertBatchElement = new XmlElement("insert");
-		insertBatchElement.addAttribute(new Attribute("id", "insertBatch"));
-		insertBatchElement.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
-		insertBatchElement.addElement(new TextElement("insert into " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
+    /**
+     * 增加批量插入的xml配置
+     *
+     * @author 吴帅
+     * @parameter @param document
+     * @parameter @param introspectedTable
+     * @createDate 2015年8月9日 下午6:57:43
+     */
+    @Override
+    public void addBatchInsertXml(Document document, IntrospectedTable introspectedTable) {
+        List<IntrospectedColumn> columns = introspectedTable.getAllColumns();
+        //获得要自增的列名
+        String incrementField = introspectedTable.getTableConfiguration().getProperties().getProperty("incrementField");
+        if (incrementField != null) {
+            incrementField = incrementField.toUpperCase();
+        }
+        StringBuilder dbcolumnsName = new StringBuilder();
+        StringBuilder javaPropertyAndDbType = new StringBuilder();
+        for (IntrospectedColumn introspectedColumn : columns) {
+            String columnName = introspectedColumn.getActualColumnName();
+            if (!columnName.toUpperCase().equals(incrementField)) {//不是自增字段的才会出现在批量插入中
+                dbcolumnsName.append(columnName + ",");
+                javaPropertyAndDbType.append("#{item." + introspectedColumn.getJavaProperty() + ",jdbcType=" + introspectedColumn.getJdbcTypeName() + "},");
+            }
+        }
 
-		XmlElement trim1Element = new XmlElement("trim");
-		trim1Element.addAttribute(new Attribute("prefix", "("));
-		trim1Element.addAttribute(new Attribute("suffix", ")"));
-		trim1Element.addAttribute(new Attribute("suffixOverrides", ","));
-		trim1Element.addElement(new TextElement(dbcolumnsName.toString()));
-		insertBatchElement.addElement(trim1Element);
+        XmlElement insertBatchElement = new XmlElement("insert");
+        insertBatchElement.addAttribute(new Attribute("id", "insertBatch"));
+        insertBatchElement.addAttribute(new Attribute("parameterType", introspectedTable.getBaseRecordType()));
+        insertBatchElement.addElement(new TextElement("insert into " + introspectedTable.getAliasedFullyQualifiedTableNameAtRuntime()));
 
-		insertBatchElement.addElement(new TextElement("values"));
+        XmlElement trim1Element = new XmlElement("trim");
+        trim1Element.addAttribute(new Attribute("prefix", "("));
+        trim1Element.addAttribute(new Attribute("suffix", ")"));
+        trim1Element.addAttribute(new Attribute("suffixOverrides", ","));
+        trim1Element.addElement(new TextElement(dbcolumnsName.toString()));
+        insertBatchElement.addElement(trim1Element);
 
-		XmlElement foreachElement = new XmlElement("foreach");
-		foreachElement.addAttribute(new Attribute("collection", "list"));
-		foreachElement.addAttribute(new Attribute("index", "index"));
-		foreachElement.addAttribute(new Attribute("item", "item"));
-		foreachElement.addAttribute(new Attribute("separator", ","));
-		foreachElement.addElement(new TextElement("("));
-		XmlElement trim2Element = new XmlElement("trim");
-		trim2Element.addAttribute(new Attribute("suffixOverrides", ","));
-		trim2Element.addElement(new TextElement(javaPropertyAndDbType.toString()));
-		foreachElement.addElement(trim2Element);
-		foreachElement.addElement(new TextElement(")"));
-		insertBatchElement.addElement(foreachElement);
+        insertBatchElement.addElement(new TextElement("values"));
 
-		document.getRootElement().addElement(insertBatchElement);
-	}
+        XmlElement foreachElement = new XmlElement("foreach");
+        foreachElement.addAttribute(new Attribute("collection", "list"));
+        foreachElement.addAttribute(new Attribute("index", "index"));
+        foreachElement.addAttribute(new Attribute("item", "item"));
+        foreachElement.addAttribute(new Attribute("separator", ","));
+        foreachElement.addElement(new TextElement("("));
+        XmlElement trim2Element = new XmlElement("trim");
+        trim2Element.addAttribute(new Attribute("suffixOverrides", ","));
+        trim2Element.addElement(new TextElement(javaPropertyAndDbType.toString()));
+        foreachElement.addElement(trim2Element);
+        foreachElement.addElement(new TextElement(")"));
+        insertBatchElement.addElement(foreachElement);
 
-	/**
-	 * 在xml文件的查询配置中加入分页支持
-	 * 
-	 * @author 吴帅
-	 * @parameter @param element
-	 * @parameter @param preFixId
-	 * @parameter @param sufFixId
-	 * @createDate 2015年9月29日 上午11:59:06
-	 */
-	@Override
-	public XmlElement adaptSelectByExample(XmlElement element, IntrospectedTable introspectedTable) {
-		XmlElement paginationElement = new XmlElement("include"); //$NON-NLS-1$   
-		paginationElement.addAttribute(new Attribute("refid", "MysqlDialectSuffix"));
-		element.getElements().add(paginationElement);
-		return element;
-	}
+        document.getRootElement().addElement(insertBatchElement);
+    }
 
-	/**
-	 * 只插入设置过的字段值,sqlserver空实现即可
-	 * 
-	 * @author 吴帅
-	 * @parameter @param element
-	 * @parameter @param introspectedTable
-	 * @createDate 2015年9月29日 下午12:00:37
-	 */
-	@Override
-	public void adaptInsertSelective(XmlElement element, IntrospectedTable introspectedTable) {
+    /**
+     * 在xml文件的查询配置中加入分页支持
+     *
+     * @author 吴帅
+     * @parameter @param element
+     * @parameter @param preFixId
+     * @parameter @param sufFixId
+     * @createDate 2015年9月29日 上午11:59:06
+     */
+    @Override
+    public XmlElement adaptSelectByExample(XmlElement element, IntrospectedTable introspectedTable) {
+        XmlElement paginationElement = new XmlElement("include"); //$NON-NLS-1$
+        paginationElement.addAttribute(new Attribute("refid", "MysqlDialectSuffix"));
+        element.getElements().add(paginationElement);
+        return element;
+    }
 
-	}
+    /**
+     * 只插入设置过的字段值,sqlserver空实现即可
+     *
+     * @author 吴帅
+     * @parameter @param element
+     * @parameter @param introspectedTable
+     * @createDate 2015年9月29日 下午12:00:37
+     */
+    @Override
+    public void adaptInsertSelective(XmlElement element, IntrospectedTable introspectedTable) {
+
+    }
 }
--- src/main/java/org/ihansen/mbp/extend/java/CustomInterface.java
+++ src/main/java/org/ihansen/mbp/extend/java/CustomInterface.java
@@ -3,19 +3,15 @@ package org.ihansen.mbp.extend.java;
 import org.mybatis.generator.api.dom.java.FullyQualifiedJavaType;
 import org.mybatis.generator.api.dom.java.Interface;
 
-public class CustomInterface extends Interface
-{
+public class CustomInterface extends Interface {
 
-	public CustomInterface(FullyQualifiedJavaType type)
-	{
-		super(type);
-	}
+    public CustomInterface(FullyQualifiedJavaType type) {
+        super(type);
+    }
 
-	public CustomInterface(String type)
-	{
-		super(type);
-	}
-	
-	
-	
+    public CustomInterface(String type) {
+        super(type);
+    }
+
+
 }
